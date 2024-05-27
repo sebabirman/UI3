8e57e2ccec663e8e09697fa2b71d762c1ec5c9a0
sebabirman <152617869+sebabirman@users.noreply.github.com>
2024-05-27 15:53:20 -0300

    password y loginmanager 2

--- Assets/Loginmanager.cs
+++ Assets/Loginmanager.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Loginmanager : MonoBehaviour
 {
+    public InputField Inputpassword;
+    public string password = "9090" ;
     // Start is called before the first frame update
-    void Start()
+    public void chequearcontraseña()
     {
-        
-    }
+        if (Inputpassword.text == password)
+        {
+            Debug.Log("acces granted");
+        }
+       
+            else
+        {
+            Debug.Log("Acces denied");
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
+    
 }
